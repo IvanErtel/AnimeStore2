b0eb427f2843abc7ba64c450fca1b862be4ac688
ivan ertel <alanertel2275@gmail.com>
2023-04-26 21:33:55 -0300

    cambio de baconsumo de productos mediante JSON

--- scripst/juguetes.js
+++ scripst/juguetes.js
@@ -7,42 +7,41 @@ fetch('../json/juguetes.json')
 
     // Estos divs van fuera del forech para que no se cree un container por cada producto
     const contDiv = document.createElement('div');
-    contDiv.classList.add('container');
+    contDiv.classList.add('container', 'card-Json');
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
 
     // Recorremos los datos obtenidos del archivo juguetes.json
     // y creamos elementos HTML para cada producto
     data.forEach(producto => {
-      // creamos un elemento div con la clase "col-md-3"
+      // creamos los elementos div 
       const colDiv = document.createElement('div');
-      colDiv.classList.add('col-md-3', 'cardCol');
+      colDiv.classList.add('col-md-3');
 
-      // creamos un elemento div con la clase "card"
       const cardDiv = document.createElement('div');
       cardDiv.classList.add('card');
 
-      // creamos un elemento img con la clase "card-img-top" para la imagen del producto
+      // creamos un elemento img para la imagen del producto
       const img = document.createElement('img');
       img.src = producto.imagen;
       img.alt = `Producto ${producto.id}`;
       img.classList.add('card-img-top');
 
-      // creamos un elemento div con la clase "card-body" para el cuerpo de la tarjeta
+      // creamos un elemento div para el cuerpo de la card
       const cardBodyDiv = document.createElement('div');
       cardBodyDiv.classList.add('card-body');
 
-      // creamos un elemento h3 con la clase "card-title" para el nombre del producto
+      // creamos un elemento h3 para el nombre del producto
       const h3 = document.createElement('h3');
       h3.textContent = producto.nombre;
       h3.classList.add('card-title');
 
-      // creamos un elemento p con la clase "card-text" para el precio del producto
+      // creamos un elemento p para el precio del producto
       const pPrecio = document.createElement('p');
       pPrecio.textContent = `Precio: $${producto.precio}`;
       pPrecio.classList.add('card-text');
 
-      // creamos un elemento button con la clase "btn" y "btn-primary" para el botón "Agregar al Carrito"
+      // creamos un elemento button para el botón "Agregar al Carrito"
       const btnAgregar = document.createElement('button');
       btnAgregar.textContent = 'Agregar al Carrito';
       btnAgregar.classList.add('btn', 'btn-primary');
